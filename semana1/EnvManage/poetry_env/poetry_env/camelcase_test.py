528e1c8261a30db69636b8436bece3ed1768ff5d
Vakihito <victor.kamada.tomita@gmail.com>
2021-07-19 21:31:34 -0300

    poetry update

---
+++ semana1/EnvManage/poetry_env/poetry_env/camelcase_test.py
@@ -0,0 +1,15 @@
+from camelcase import CamelCase
+import pandas as pd
+
+s = 'sentiment analisys using textual and visual features'
+
+print()
+print( s )
+print()
+print()
+print( CamelCase().hump(s) )
+print()
+
+
+
+
